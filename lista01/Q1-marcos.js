12ea99ddfee094ec9db220e5b6ce64632497aefe
Marcos Vinicius <viniciuslag@gmail.com>
2018-08-10 12:23:27 -0300

    Questão 1 resolvida por Function

---
+++ lista01/Q1-marcos.js
@@ -0,0 +1,7 @@
+
+function conversor(a){
+    console.log(a +'m Em decímetros é ' + a*10 +'dm')
+    console.log(a + 'm Em centimétros é ' + a*100 + 'cm')
+    console.log(a + 'm Em milímetros é ' + a*1000)
+}
+conversor(1)

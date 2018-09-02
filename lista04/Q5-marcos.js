4222d0a3fbf763a35c83cb9feddc25b7c969ddff
marcmec <viniciuslag@gmail.com>
2018-09-02 03:45:09 -0300

    Questão 05 finalizada

---
+++ lista04/Q5-marcos.js
@@ -0,0 +1,9 @@
+//mostrar o quadrado de cada número de 1 a 1000
+
+let contador1=0,contador2=0
+
+for(a=0;a<1000;a++){
+    contador1++
+    console.log(contador1+' o quadrado dele é: '+(docontador2=Math.pow(contador1,2)))
+
+}

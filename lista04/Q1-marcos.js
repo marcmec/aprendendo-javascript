37a3a340679f3344b61b3bf7fad666b41e161bd4
marcmec <viniciuslag@gmail.com>
2018-09-02 01:31:27 -0300

    Questão 01 finalizada com algoritmo repetido

--- lista04/Q1-marcos.js
+++ lista04/Q1-marcos.js
@@ -1,4 +1,4 @@
-//ler 5 valores
+//ler 5 valores e distinguir quem é par quem é ímpar quem é positivo ou negativo
 
 
 
@@ -8,8 +8,71 @@ let valores= (a1,a2,a3,a4,a5) =>{
     let contpositivo=0
     let contnegativo=0
 
-    if(a1){
-
-    }
-
+    if(a1%2==0){
+        contopar++
+        if(a1<0){
+            contnegativo++
+        }else{
+        contpositivo++
+    } 
 }
+if(a1%2==1){
+    contimpar++
+    contpositivo++
+   
+}else  if(a1%2==-1){
+    contimpar++
+    contnegativo++
+}//
+   if(a2%2==0){
+        contopar++
+        if(a2<0){
+            contnegativo++
+        }else{
+        contpositivo++
+    } 
+}
+if(a3%2==1){
+    contimpar++
+    contpositivo++
+   
+}else  if(a3%2==-1){
+    contimpar++
+    contnegativo++
+}//
+if(a4%2==0){
+    contopar++
+    if(a4<0){
+        contnegativo++
+    }else{
+    contpositivo++
+} 
+}
+if(a4%2==1){
+contimpar++
+contpositivo++
+
+}else  if(a4%2==-1){
+contimpar++
+contnegativo++
+}//
+if(a5%2==0){
+    contopar++
+    if(a5<0){
+        contnegativo++
+    }else{
+    contpositivo++
+} 
+}
+if(a5%2==1){
+contimpar++
+contpositivo++
+
+}else  if(a5%2==-1){
+contimpar++
+contnegativo++
+}//
+console.log(contopar,contpositivo,contimpar,contnegativo)
+}
+
+valores(-3,2,5,4,8)//informe os 5 valores para serem avaliados aqui

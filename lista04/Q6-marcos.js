34c1f0ca26c64a6e35a5ad53719882f5c736de7c
marcmec <viniciuslag@gmail.com>
2018-09-02 03:53:50 -0300

    Questão 06 finalizada

---
+++ lista04/Q6-marcos.js
@@ -0,0 +1,13 @@
+//apresentar todos os números divisíveis por 5 maiores que 0 até 200 inclusive
+
+
+let impares
+
+
+for(a=0;a<201;a++){
+
+    if(a%5==0 && a>0){
+        
+        console.log(impares=a)
+    }
+}

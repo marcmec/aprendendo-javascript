e6dbd2407038e8b58ee281e5e6503121ab378bda
marcmec <viniciuslag@gmail.com>
2018-09-08 19:19:27 -0300

    Linha de código de inserção de valores adicionada

--- lista01/Q6-marcos.js
+++ lista01/Q6-marcos.js
@@ -1,8 +1,8 @@
 //código para saber idade
 
-let anos=(nascimento)=> (
-
+let anos=()=> {
+    let nascimento= parseInt(window.prompt("Digite o ano de nascimento: "))
     console.log('Eu tenho ' + (2018-nascimento) +' anos, e em 2021 terei '+ (2021-nascimento) )
-)
+}
 
-anos(1995)
+anos()

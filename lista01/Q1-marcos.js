50e7d1f02b49796a36236bec68242fe07caf6bf2
marcmec <viniciuslag@gmail.com>
2018-09-08 18:44:27 -0300

    Acréscimo de linha para entrada de valores

--- lista01/Q1-marcos.js
+++ lista01/Q1-marcos.js
@@ -1,7 +1,10 @@
 //conversor comprimento
-function conversor(a){
+//testar esses códigos em navegadores
+
+
+let a=parseInt(window.prompt("Digite o valor em centímetros!"))
+
     console.log(a +'m Em decímetros é ' + a*10 +'dm')
     console.log(a + 'm Em centimétros é ' + a*100 + 'cm')
     console.log(a + 'm Em milímetros é ' + a*1000)
-}
-conversor(1)
+
---
+++ lista05/Q1-marcos.js
@@ -0,0 +1,15 @@
+//do while sem usar o operador ||
+
+
+
+let nota1=11
+let a=true
+
+while(a){
+
+    console.log(nota1)
+
+    if(nota1===11){
+        a=false
+    }
+}

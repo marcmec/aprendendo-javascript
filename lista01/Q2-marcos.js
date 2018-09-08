37d2ff85e6ba428bd5f23415dac8c6623dc8583c
marcmec <viniciuslag@gmail.com>
2018-09-08 18:57:59 -0300

    Lista 1 questão 02 com inserção de valores

--- lista01/Q2-marcos.js
+++ lista01/Q2-marcos.js
@@ -1,7 +1,11 @@
 //soma de números
+//testar esses códigos em navegadores
 
-let soma=(a,b,c,d) => (
-    console.log('a soma dos números é : ' + `${a+b+c+d}`)
-)
 
-soma(1,5,2,2)
+    let a= parseInt(window.prompt("Insira o valor de A: "))
+    let b= parseInt(window.prompt("Insira o valor de B: "))
+    let c= parseInt(window.prompt("Insira o valor de C: "))
+    let d= parseInt(window.prompt("Insira o valor de D: "))
+
+    console.log(a+b+c+d)
+    alert(a+b+c+d)

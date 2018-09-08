ffac6746b416d866e6629269ab187c37ca255909
marcmec <viniciuslag@gmail.com>
2018-09-08 19:03:00 -0300

    Lista 01 questão 03 com inserção de valores

--- lista01/Q3-marcos.js
+++ lista01/Q3-marcos.js
@@ -1,6 +1,8 @@
 //media de 3 numeros
-let mediaTres= (a,b,c) => (
-
-    console.log('a média aritmética é: ' + `${(a+b+c)/3}`)
-)
-mediaTres(1,4,1)
+let mediaTres= () => {
+    let a= parseInt(window.prompt("Insira o valor de A: "))
+    let b= parseInt(window.prompt("Insira o valor de B: "))
+    let c= parseInt(window.prompt("Insira o valor de C: "))
+    console.log('a média aritmética é: ' + (a+b+c)/3)
+}
+mediaTres()

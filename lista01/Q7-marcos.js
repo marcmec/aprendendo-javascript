126d6247233113ebf3137378f7e4dce8dade1dda
marcmec <viniciuslag@gmail.com>
2018-09-08 19:21:31 -0300

    Linha de código para inserir valor adicionada

--- lista01/Q7-marcos.js
+++ lista01/Q7-marcos.js
@@ -3,7 +3,7 @@
 Código para converter segundos em hora exata
 */
 
-let s =7322
+let s =parseInt(window.prompt("Digite os segundos: ex: 7322s"))
 h=Math.floor(s/3600)
 resthour=s%3600
 min=Math.floor(resthour/60)

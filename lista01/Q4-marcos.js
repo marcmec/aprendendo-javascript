451ab1781ca97279e6b67f611ad4af3a5984547c
marcmec <viniciuslag@gmail.com>
2018-09-08 19:16:42 -0300

    Linha para inserção de valores adicionadas

--- lista01/Q4-marcos.js
+++ lista01/Q4-marcos.js
@@ -3,7 +3,7 @@ var z=3,x=2,s=4,v=5
 
 let ponderada=(a,b,c,d) => (
     
-    console.log('a média ponderada dos valores são: ' + `${(a*z+b*x+c*s+d*v)/(3+2+4+5)}`)
+    console.log('a média ponderada dos valores são: ' + `${Math.floor((a*a+b*b+c*c+d*d)/(a+b+c+d))}`)
 )
 
 ponderada(3,2,4,4)
--- lista01/Q5-marcos.js
+++ lista01/Q5-marcos.js
@@ -1,9 +1,12 @@
 //Código para exibir resultados lógicos
 
-let maiorque=(a) => (
+let maiorque=() => {
+    
+
+    let a= parseInt(window.prompt("Insira o valor de A: "))
 
     console.log( a + ' ao quadrado é '+ Math.pow(a,2) + ' ao cubo é : ' + Math.pow(a,3) + ' raiz quadrada é : ' + Math.sqrt(a) + ' raiz cúbica é : ' + Math.cbrt(a))
     
     
-)
-maiorque(8)
+}
+maiorque()

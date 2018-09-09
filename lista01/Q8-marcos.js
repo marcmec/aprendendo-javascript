9cdcab53c89b9bf620abda6fad7c94a8dd75d4cf
marcmec <viniciuslag@gmail.com>
2018-09-08 23:27:40 -0300

    Linha de código para inserir Valoreces adicionadas

--- lista01/Q8-marcos.js
+++ lista01/Q8-marcos.js
@@ -1,8 +1,7 @@
 //código para conversão de temperatura
-let celsius= (f) =>(
-
+let celsius= () =>{
+    let f= parseInt(window.prompt("Digite uma temperatura em farenheight"))
     console.log(5*((f-32)/+9))
 
-)
-
-celsius(212)//informe o valor em fahrenheit
+}
+celsius()//informe o valor em fahrenheit

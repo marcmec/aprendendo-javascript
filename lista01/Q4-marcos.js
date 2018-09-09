6e07ef54e22841208246d91a9bf4d558e31b9ac2
marcmec <viniciuslag@gmail.com>
2018-09-09 20:15:09 -0300

    WINDOW.PROMPT

--- lista01/Q4-marcos.js
+++ lista01/Q4-marcos.js
@@ -1,9 +1,11 @@
 //media ponderada
-var z=3,x=2,s=4,v=5
 
-let ponderada=(a,b,c,d) => (
-    
-    console.log('a média ponderada dos valores são: ' + `${Math.floor((a*a+b*b+c*c+d*d)/(a+b+c+d))}`)
-)
 
-ponderada(3,2,4,4)
+let ponderada=() => {
+    let a= parseInt(window.prompt("Insira o valor de A: "))
+    let b= parseInt(window.prompt("Insira o valor de B: "))
+    let c= parseInt(window.prompt("Insira o valor de C: "))
+    let d= parseInt(window.prompt("Insira o valor de D: "))
+    console.log('a média ponderada dos valores são: ' + `${(a*a+b*b+c*c+d*d)/(a+b+c+d)}`)
+}
+ponderada()

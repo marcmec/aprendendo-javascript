817b6c805d5f05f67d3214a671cd5f399d22f255
marcmec <viniciuslag@gmail.com>
2018-09-09 01:54:01 -0300

    window.prompt add

--- lista01/Q13-marcos.js
+++ lista01/Q13-marcos.js
@@ -2,11 +2,11 @@
 let eleitores=parseInt(window.prompt("Digite a quantidade de eleitores"))
 let brancos= parseInt(window.prompt("Digite quantos votos foram brancos"))
 let nulos= parseInt(window.prompt("Digite quantos votos foram nulos"))
+let validos= parseInt(window.prompt("Digite quantos votos foram validos"))
 
 
 
-
+console.log('% dos válidos é :' +(validos*100)/eleitores)
 console.log('% dos brancos é :' +(brancos*100)/eleitores)
-
 console.log('% dos nulosé :'+ (nulos*100)/eleitores)
 

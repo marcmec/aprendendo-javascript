003cc9defb39d904ea53af48e53946927c7c9eba
marcmec <viniciuslag@gmail.com>
2018-09-09 01:18:39 -0300

    Linha de código para inserir valor

--- lista01/Q9-marcos.js
+++ lista01/Q9-marcos.js
@@ -1,8 +1,8 @@
 //código para transformar mm em polegadas
 
-let chuva= (polegar) =>(
-
+let chuva= () =>{
+    let polegar= parseInt(window.prompt("Insira o valor em polegadas"))
     console.log('a quantidade de ' + polegar+ ' polegadas em milímetros é :' + (polegar*25.4)+ ' milímetros')
-)
+}
 
-chuva(3)
+chuva()
